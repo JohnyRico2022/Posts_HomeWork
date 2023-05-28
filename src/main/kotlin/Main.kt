de9bfc5a428e35824f4d5a49b4e1f6e3378667ad
Nikita Konstantinov <Nik-kita89@yandex.ru>
2023-05-28 21:59:42 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,88 @@
+import WallService.posts
+
+data class Post(
+    val id: Int, // Идентификатор записи
+    val ownerId: Int, //Идентификатор владельца стены, на которой размещена запись
+    val fromId: Int, //Идентификатор автора записи
+    val data: Int, //Время публикации записи в формате unixtime
+    val text: String, //текст записи
+    val canPin: Boolean, //Информация, может ли текущий пользователь закрепить запись
+    val canDelete: Boolean, //Информация, может ли текущий пользователь удалить запись
+    val canEdit: Boolean, //Информация, может ли текущий пользователь редактировать запись
+    val isFavorite: Boolean, //Добавлен ли объект в закладки у текущего пользователя
+    val likes: Likes
+) {
+
+    operator fun set(index: Int, value: Post) {
+        posts[index] = value
+    }
+}
+
+
+object Likes {
+    val count: Int = 0  // число пользователей которым понравилась запись
+    val userLikes: Boolean = false // наличие метки "Мне нравится" от текущего пользователя
+    val canLike: Boolean = true // информация, может ли текущий пользователь поставить отметку «Мне нравится»
+    val canPublish: Boolean = true // информация, может ли текущий пользователь сделать репост записи
+
+    override fun toString(): String {
+
+        return "Лайки от пользователей: $count"
+    }
+}
+
+
+object WallService {
+
+    private var postId: Int = 0
+    var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+
+        val postUniqueId = postId + 1
+        posts += post
+        postId++
+        return posts[postUniqueId - 1]
+    }
+
+    fun update(post: Post): Boolean {
+        val checkedId = post.id
+        for ((index, post) in posts.withIndex())
+            if (checkedId == post.id) {
+
+                posts[index] = post.copy(99, 99, 99, 99999, "new post", false, false, false, false, Likes)
+                return true
+            }
+        return false
+    }
+
+    fun clear() {
+        posts = emptyArray()
+    }
+}
+
+
+fun main() {
+
+    val like1 = Likes
+
+    val post1 = Post(1, 31, 50, 1685030715, "Привет, Мир1!", true, true, false, false, like1)
+    val post2 = Post(2, 32, 60, 1685030716, "Привет, Мир2!", true, true, false, false, like1)
+    val post3 = Post(3, 33, 70, 1685030717, "Привет, Мир3!", true, true, false, false, like1)
+    val post4 = Post(4, 34, 80, 1685030717, "Привет, Мир4!", true, true, false, false, like1)
+
+    val postTest = Post(2, 34, 80, 1685030717, "Привет, Мир4!", true, true, false, false, like1)
+
+    WallService.add(post1)
+    WallService.add(post2)
+    WallService.add(post3)
+    WallService.add(post4)
+
+    println("Размер массива с постами : ${WallService.posts.size}")
+    println("************")
+    println(posts.joinToString())
+    println("#############")
+    WallService.update(postTest)
+    println(posts.joinToString())
+
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,54 @@
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class WallServiceTest {
+
+
+    @Test
+    fun add() {
+        var postId: Int = 0
+
+        val postUniqueId = postId + 1
+
+        assertEquals(1, postUniqueId)
+
+    }
+
+
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
+
+    @Test
+    fun updateTrue() {
+        val like1 = Likes
+        var posts = emptyArray<Post>()
+
+        val post1 = Post(1, 31, 50, 1685030715, "Привет, Мир1!", true, true, false, false, like1)
+        val post2 = Post(2, 32, 60, 1685030716, "Привет, Мир2!", true, true, false, false, like1)
+        val post3 = Post(3, 33, 70, 1685030717, "Привет, Мир3!", true, true, false, false, like1)
+        val post4 = Post(4, 34, 80, 1685030717, "Привет, Мир4!", true, true, false, false, like1)
+        val postTest = Post(2, 34, 80, 1685030717, "Привет, Мир4!", true, true, false, false, like1)
+
+
+        assertEquals(true, true)
+    }
+
+    @Test
+    fun updateFalse() {
+        val like1 = Likes
+        var posts = emptyArray<Post>()
+
+        val post1 = Post(1, 31, 50, 1685030715, "Привет, Мир1!", true, true, false, false, like1)
+        val post2 = Post(2, 32, 60, 1685030716, "Привет, Мир2!", true, true, false, false, like1)
+        val post3 = Post(3, 33, 70, 1685030717, "Привет, Мир3!", true, true, false, false, like1)
+        val post4 = Post(4, 34, 80, 1685030717, "Привет, Мир4!", true, true, false, false, like1)
+        val postTest = Post(7, 34, 80, 1685030717, "Привет, Мир4!", true, true, false, false, like1)
+
+
+        assertEquals(false, false)
+    }
+}
